63c6fba2912f7773016a50bd0976de1e66d960a4
Angshuman44 <mandalzzme@gmail.com>
2024-03-18 19:04:12 +0530

    Prediction Pipeline bhi done

--- app.py
+++ app.py
@@ -1,26 +1,50 @@
-from src.ML_PROJECT.logger import logging
-from src.ML_PROJECT.exception import CustomException
-from src.ML_PROJECT.components.data_ingestion import DataIngestion,DataIngestionConfig
-from src.ML_PROJECT.components.data_transformation import DataTransformationConfig,DataTransformation
-from src.ML_PROJECT.components.model_trainer import ModelTrainer,ModelTrainerConfig
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
 
-import sys
+from sklearn.preprocessing import StandardScaler
+from src.ML_PROJECT.pipelines.prediction_pipeline import CustomData,PredictPipeline
+
+app = Flask(__name__)
 
 
+#Route for Home
 
-if __name__=='__main__':
-    logging.info("Execution ho gaya start bhai")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    try:
-        #data_ingestion_config=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion(data='artifacts/raw.csv')
 
-        #data_transformation_config=DataTransformationConfig()
-        data_transformation=DataTransformation()
-        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
-        model_trainer=ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys) 
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        print(results)
+
+        return render_template('home.html',predicted_result=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True)        
